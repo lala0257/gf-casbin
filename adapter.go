c684caa2c9dca7af04813abaebfb94fc86e2e3d3
lala0257 <chenyu0257@163.com>
2022-12-29 17:07:43 +0800

    gdb adapter

--- adapter.go
+++ adapter.go
@@ -3,14 +3,13 @@ package gf_casbin
 import (
 	"context"
 	"fmt"
-	"github.com/casbin/casbin/v2"
 	"github.com/casbin/casbin/v2/model"
 	"github.com/casbin/casbin/v2/persist"
 	"github.com/gogf/gf/v2/database/gdb"
+	"runtime"
 )
 
-const DefaultTableName = "casbin_rule"
-
+// CasbinRule is the golang structure for table casbin_rule.
 type CasbinRule struct {
 	Ptype string `json:"ptype" ` //
 	V0    string `json:"v0"    ` //
@@ -21,71 +20,113 @@ type CasbinRule struct {
 	V5    string `json:"v5"    ` //
 }
 
-type GfCasbin struct {
-	Enforcer    *casbin.SyncedEnforcer
-	EnforcerErr error
-	modelFile   string
-	ctx         context.Context
-	db          gdb.DB
+// Adapter 用于策略存储的gdb适配器。
+type Adapter struct {
+	Link      string // 链接到数据库的信息，例如: "mysql:root:12345678@tcp(127.0.0.1:3306)/test"
+	TableName string // 存储策略的表
+	DB        gdb.DB // 操作数据库的ORM句柄
+	ctx       context.Context
 }
 
-// New casbin适配器
-func New(ctx context.Context, modelFile string, db gdb.DB) *GfCasbin {
-	a := &GfCasbin{
-		modelFile: modelFile,
+// finalizer is the destructor for Adapter.
+func finalizer(a *Adapter) {
+	// 注意不用的时候不需要使用Close方法关闭数据库连接(并且gdb也没有提供Close方法)，
+	// 数据库引擎底层采用了链接池设计，当链接不再使用时会自动关闭
+	a.DB = nil
+}
+
+// NewAdapter is the constructor for Adapter.
+func NewAdapter(ctx context.Context, link string) (*Adapter, error) {
+	a := &Adapter{
+		Link:      link,
+		TableName: "casbin_rule",
+		ctx:       ctx,
 	}
-	a.initPolicy(ctx, db)
-	return a
+	// Open the DB, create it if not existed.
+	if err := a.open(); err != nil {
+		return nil, err
+	}
+
+	// Call the destructor when the object is released.
+	runtime.SetFinalizer(a, finalizer)
+
+	return a, nil
 }
 
-func (a *GfCasbin) DbCtx() *gdb.Model {
-	return a.db.Model(DefaultTableName).Safe().Ctx(a.ctx)
-}
+func (a *Adapter) open() error {
+	gdb.SetConfig(gdb.Config{
+		"casbin": gdb.ConfigGroup{
+			gdb.ConfigNode{
+				Link:   a.Link,
+				Role:   "master",
+				Weight: 100,
+			},
+		},
+	})
 
-func (a *GfCasbin) initPolicy(ctx context.Context, db gdb.DB) {
-	e, err := casbin.NewSyncedEnforcer(a.modelFile, a)
+	db, err := gdb.NewByGroup("casbin")
 	if err != nil {
-		a.EnforcerErr = err
-		return
+		return err
 	}
-	e.ClearPolicy()
-	a.Enforcer = e
-	a.ctx = ctx
-	a.db = db
-	// Load the policy from DB.
-	err = a.LoadPolicy(e.GetModel())
-	if err != nil {
-		a.EnforcerErr = err
-		return
-	}
+	a.DB = db
+
+	return a.createTable()
+}
+
+func (a *Adapter) close() error {
+	// 注意不用的时候不需要使用Close方法关闭数据库连接(并且gdb也没有提供Close方法)，
+	// 数据库引擎底层采用了链接池设计，当链接不再使用时会自动关闭
+	a.DB = nil
+	return nil
+}
+
+func (a *Adapter) createTable() error {
+	_, err := a.DB.Exec(a.ctx, fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s (ptype VARCHAR(10), v0 VARCHAR(256), v1 VARCHAR(256), v2 VARCHAR(256), v3 VARCHAR(256), v4 VARCHAR(256), v5 VARCHAR(256))", a.TableName))
+	return err
+}
+
+func (a *Adapter) dropTable() error {
+	_, err := a.DB.Exec(a.ctx, fmt.Sprintf("DROP TABLE %s", a.TableName))
+	return err
+}
+
+func (a *Adapter) DbCtx() *gdb.Model {
+	return a.DB.Model(a.TableName).Safe().Ctx(a.ctx)
 }
 
 // LoadPolicy loads policy from database.
-func (a *GfCasbin) LoadPolicy(model model.Model) error {
+func (a *Adapter) LoadPolicy(model model.Model) error {
 	var lines []*CasbinRule
+
 	if err := a.DbCtx().Scan(&lines); err != nil {
 		return err
 	}
+
 	for _, line := range lines {
-		a.loadPolicyLine(line, model)
+		err := loadPolicyLine(line, model)
+		if err != nil {
+			return err
+		}
 	}
+
 	return nil
 }
 
 // SavePolicy saves policy to database.
-func (a *GfCasbin) SavePolicy(model model.Model) (err error) {
-	err = a.dropTable()
+func (a *Adapter) SavePolicy(model model.Model) error {
+	err := a.dropTable()
 	if err != nil {
-		return
+		return err
 	}
 	err = a.createTable()
 	if err != nil {
-		return
+		return err
 	}
+
 	for ptype, ast := range model["p"] {
 		for _, rule := range ast.Policy {
-			line := a.savePolicyLine(ptype, rule)
-			_, err := a.DbCtx().Data(line).Insert()
+			line := savePolicyLine(ptype, rule)
+			_, err := a.DbCtx().Data(&line).Insert()
 			if err != nil {
 				return err
 			}
@@ -94,41 +135,35 @@ func (a *GfCasbin) SavePolicy(model model.Model) (err error) {
 
 	for ptype, ast := range model["g"] {
 		for _, rule := range ast.Policy {
-			line := a.savePolicyLine(ptype, rule)
-			_, err := a.DbCtx().Data(line).Insert()
+			line := savePolicyLine(ptype, rule)
+			_, err := a.DbCtx().Data(&line).Insert()
 			if err != nil {
 				return err
 			}
 		}
 	}
-	return
-}
 
-func (a *GfCasbin) dropTable() (err error) {
-	_, err = a.db.Exec(a.ctx, fmt.Sprintf("DROP TABLE %s", DefaultTableName))
-	return err
-}
-
-func (a *GfCasbin) createTable() (err error) {
-	_, err = a.db.Exec(a.ctx, fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s (ptype VARCHAR(10), v0 VARCHAR(256), v1 VARCHAR(256), v2 VARCHAR(256), v3 VARCHAR(256), v4 VARCHAR(256), v5 VARCHAR(256))", DefaultTableName))
-	return err
+	return nil
 }
 
 // AddPolicy adds a policy rule to the storage.
-func (a *GfCasbin) AddPolicy(sec string, ptype string, rule []string) error {
-	line := a.savePolicyLine(ptype, rule)
-	_, err := a.DbCtx().Data(line).Insert()
+func (a *Adapter) AddPolicy(sec string, ptype string, rule []string) error {
+	line := savePolicyLine(ptype, rule)
+	_, err := a.DbCtx().Data(&line).Insert()
 	return err
 }
 
-func (a *GfCasbin) RemovePolicy(sec string, ptype string, rule []string) error {
-	line := a.savePolicyLine(ptype, rule)
-	err := a.rawDelete(line)
+// RemovePolicy removes a policy rule from the storage.
+func (a *Adapter) RemovePolicy(sec string, ptype string, rule []string) error {
+	line := savePolicyLine(ptype, rule)
+	err := rawDelete(a, line)
 	return err
 }
 
-func (a *GfCasbin) RemoveFilteredPolicy(sec string, ptype string, fieldIndex int, fieldValues ...string) error {
+// RemoveFilteredPolicy removes policy rules that match the filter from the storage.
+func (a *Adapter) RemoveFilteredPolicy(sec string, ptype string, fieldIndex int, fieldValues ...string) error {
 	line := &CasbinRule{}
+
 	line.Ptype = ptype
 	if fieldIndex <= 0 && 0 < fieldIndex+len(fieldValues) {
 		line.V0 = fieldValues[0-fieldIndex]
@@ -148,11 +183,11 @@ func (a *GfCasbin) RemoveFilteredPolicy(sec string, ptype string, fieldIndex int
 	if fieldIndex <= 5 && 5 < fieldIndex+len(fieldValues) {
 		line.V5 = fieldValues[5-fieldIndex]
 	}
-	err := a.rawDelete(line)
+	err := rawDelete(a, line)
 	return err
 }
 
-func (a *GfCasbin) loadPolicyLine(line *CasbinRule, model model.Model) {
+func loadPolicyLine(line *CasbinRule, model model.Model) error {
 	lineText := line.Ptype
 	if line.V0 != "" {
 		lineText += ", " + line.V0
@@ -172,11 +207,13 @@ func (a *GfCasbin) loadPolicyLine(line *CasbinRule, model model.Model) {
 	if line.V5 != "" {
 		lineText += ", " + line.V5
 	}
-	_ = persist.LoadPolicyLine(lineText, model)
+
+	return persist.LoadPolicyLine(lineText, model)
 }
 
-func (a *GfCasbin) savePolicyLine(ptype string, rule []string) *CasbinRule {
+func savePolicyLine(ptype string, rule []string) *CasbinRule {
 	line := &CasbinRule{}
+
 	line.Ptype = ptype
 	if len(rule) > 0 {
 		line.V0 = rule[0]
@@ -196,29 +233,32 @@ func (a *GfCasbin) savePolicyLine(ptype string, rule []string) *CasbinRule {
 	if len(rule) > 5 {
 		line.V5 = rule[5]
 	}
+
 	return line
 }
 
-func (a *GfCasbin) rawDelete(line *CasbinRule) error {
-	db := a.DbCtx().Where("ptype = ?", line.Ptype)
+func rawDelete(a *Adapter, line *CasbinRule) error {
+
+	m := a.DbCtx().Where("ptype = ?", line.Ptype)
 	if line.V0 != "" {
-		db = db.Where("v0 = ?", line.V0)
+		m = m.Where("v0 = ?", line.V0)
 	}
 	if line.V1 != "" {
-		db = db.Where("v1 = ?", line.V1)
+		m = m.Where("v1 = ?", line.V1)
 	}
 	if line.V2 != "" {
-		db = db.Where("v2 = ?", line.V2)
+		m = m.Where("v2 = ?", line.V2)
 	}
 	if line.V3 != "" {
-		db = db.Where("v3 = ?", line.V3)
+		m = m.Where("v3 = ?", line.V3)
 	}
 	if line.V4 != "" {
-		db = db.Where("v4 = ?", line.V4)
+		m = m.Where("v4 = ?", line.V4)
 	}
 	if line.V5 != "" {
-		db = db.Where("v5 = ?", line.V5)
+		m = m.Where("v5 = ?", line.V5)
 	}
-	_, err := db.Delete()
+
+	_, err := m.Delete()
 	return err
 }
